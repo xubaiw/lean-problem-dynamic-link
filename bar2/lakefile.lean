875a8be23c15ae0a81f2f76c54695dd980145f4c
Xubai Wang <self@xubai.wang>
2022-03-06 23:19:29 +0800

    use cross platform sharedLibExt

--- bar2/lakefile.lean
+++ bar2/lakefile.lean
@@ -4,7 +4,8 @@ open System Lake DSL
 package bar2 (pkgDir) (args) do
   let current ← IO.currentDir
   let dylibDir := current / ".." / "bar1" / "build" / "lib" 
-  let dylib := dylibDir / "libbar1.dylib"
+  let dylib := dylibDir / s!"libbar1.{sharedLibExt}"
+  IO.println s!"link to {dylib}"
   return {
     name := `bar2
     moreLibTargets := #[
